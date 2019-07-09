1709dc1ceb02cd57d53bfe2ff05c28f59d40f5c8
PaulaMihalcea <paula.mihalcea@live.com>
2019-07-09 17:24:42 +0200

    Changed saved data format
    Major changes:
    - Retrieved data is now saved in a human-readable format, similar to a Python dictionary
    - The data_rw_script.py file can be used to easily read saved data and manipulate it as a Pandas DataFrame
    
    Minor changes:
    - The data_test file shows the above cited human-readable format used in the actual data file (which is empty)
    - Added a few print() to connect.py for debug purposes

--- connect.py
+++ connect.py
@@ -23,58 +23,59 @@ time_between_data_requests = f.getint('wait_time', 'time_between_data_requests')
 period = f.getint('wait_time', 'period') / 1000
 
 config_handles = []
-for item in f.items("config_handles"):
+for item in f.items('config_handles'):
     config_handles.append(item[1])
 
 config_values = []
-for item in f.items("config_values"):
+for item in f.items('config_values'):
     config_values.append(item)
 
 data_handles = []
-for item in f.items("data_handles"):
+for item in f.items('data_handles'):
     data_handles.append(item)
 
 data_length = []
-for item in f.items("data_length"):
+for item in f.items('data_length'):
     data_length.append(item[1])
 
 # Connection
-child = pexpect.spawn(
-    'gatttool -I')  # Runs gatttool with the interactive option and returns the child process handle needed for further commands
-child.sendline(
-    'connect {0}'.format(device_mac))  # connect 54:6C:0E:80:3F:01 (sends the string to the spawned process (gatttool))
-child.expect('Connection successful',
-             connection_timeout)  # Waits for the "Connection successful" gatttool message; timeouts in the specified time
+child = pexpect.spawn('gatttool -I')  # Runs gatttool with the interactive option and returns the child process handle needed for further commands
+child.sendline('connect {0}'.format(device_mac))  # connect 54:6C:0E:80:3F:01 (sends the string to the spawned process (gatttool))
+print('Connecting to ' + str(device_mac) + '...')
+child.expect('Connection successful', connection_timeout)  # Waits for the "Connection successful" gatttool message; timeouts in the specified time
+print('Connection successful.')
 
 # Sensor configuration
+print('Starting sensor configuration...')
 for s in range(0, len(config_handles)):
     child.sendline('char-write-cmd {0} {1}'.format(config_handles[s], config_values[s][1]))  # char-write-cmd 0x00HH 0xCCCC (where HH is the BLE characteristic handle, while CC the configuration code; activates the desired sensor)
-    time.sleep(
-        time_between_sensor_configs)  # Waits some seconds before sending next command to avoid flooding the BLE device
+    time.sleep(time_between_sensor_configs)  # Waits some seconds before sending next command to avoid flooding the BLE device
+print('Sensor configuration successful.')
 
-time.sleep(
-    wait_after_config)  # Waits a few seconds (recommended) to allow the device to turn its sensors on; if sleep == 0, the first readings might be wrong as the sensors would still be off
+time.sleep(wait_after_config)  # Waits a few seconds (recommended) to allow the device to turn its sensors on; if sleep == 0, the first readings might be wrong as the sensors would still be off
 
 # Data retrieval cycle
+print('Data retrieval cycle started.')
 while True:
 
+    t = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # Current date and time in human-readable format
+    raw_data = {t: {}}  # Temporarily saves the new data as a dictionary
+
     parser = ConfigParser()
-    t = datetime.now().isoformat()  # Current date and time are in ISO 8601 standard format
     parser.add_section(str(t))  # Adds a new section to the data file, named after the current date and time
 
     for s in range(0, len(config_handles)):  # Data retrieval
-        child.sendline('char-read-hnd {0}'.format(
-            data_handles[s][1]))  # char-read-hnd 0x00HH (where HH is the BLE characteristic handle)
+        child.sendline('char-read-hnd {0}'.format(data_handles[s][1]))  # char-read-hnd 0x00HH (where HH is the BLE characteristic handle)
 
         child.expect('Characteristic value/descriptor: ', data_timeout)  # Waits for the first part of the output...
         child.expect("\r\n", data_timeout)  # ...then waits for the end of line
 
-        parser.set(str(t), str(data_handles[s][0]), str(child.before[0:int(data_length[s])]))  # Adds a new option to the newly created section, named after the current sensor and with the received data as its value
+        raw_data[t][str(data_handles[s][0])] = str(child.before[0:int(data_length[s])])  # Adds a new sensor-value pair to the temporary dictionary
 
         time.sleep(time_between_data_requests)  # Waits before proceeding with the next sensor
 
-    f = open('data', 'a')  # Opens the data file
-    parser.write(f)  # Writes retrieved data to file
-    f.close()  # Closes the data file
+    with open('data', 'a') as f:
+        f.write(str(raw_data)[1:-1])  # Writes to the data file the raw data in a human-readable format
+        f.write('\n')  # Adds a new line for the next reading
 
     time.sleep(period)  # Repeats the data reading cycle every _period_ milliseconds
